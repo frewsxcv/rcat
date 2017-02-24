5477349170061ab0182d164ded3ecf8033792600
Corey Farwell <coreyf@rwell.org>
2017-02-24 09:00:32 -0500

    Finish basic implementation.

--- main.rs
+++ main.rs
@@ -4,19 +4,32 @@ use std::io::{Read, Write};
 fn main() {
     let mut args = env::args_os();
     let name = args.next().unwrap();
-    let iter = args
-        .map(|a| path::PathBuf::from(a));
+    let iter = args.map(|a| path::PathBuf::from(a));
     for path in iter {
         if !path.is_file() {
+            writeln!(io::stderr(),
+                     "{}: {}: not a file",
+                     name.to_string_lossy(),
+                     path.to_string_lossy())
+                .expect("could not write to stderr");
             continue;
         }
-        let file = io::BufReader::new(fs::File::open(path).unwrap());
-        println!("filename: ");
-        for byte in file.bytes() {
-            write!(io::stdout(), "\\x{:x}", byte.unwrap());
+        let file = match fs::File::open(&path) {
+            Ok(f) => f,
+            Err(e) => {
+                writeln!(io::stderr(),
+                         "{}: {}: {}",
+                         name.to_string_lossy(),
+                         path.to_string_lossy(),
+                         e)
+                    .expect("could not write to stderr");
+                continue;
+            }
+        };
+        let buf_reader = io::BufReader::new(file);
+        for byte in buf_reader.bytes() {
+            write!(io::stdout(), "\\x{:x}", byte.unwrap()).expect("could not write to stdout");
         }
-        write!(io::stdout(), "\n");
-
-        //println!("{:x}", args.as_bytes());
     }
+    write!(io::stdout(), "\n").expect("could not write to stdout");
 }
