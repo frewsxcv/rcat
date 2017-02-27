26decaa8f2aa8cb798f3c9370952bd50943da5d8
Corey Farwell <coreyf@rwell.org>
2017-02-26 23:27:27 -0500

    Use buffered writer.

--- main.rs
+++ main.rs
@@ -22,19 +22,18 @@ fn print_error(error: Box<error::Error>) {
     writeln!(io::stderr(), "hexcat: {}", error.description()).expect("could not write to stderr");
 }
 
-fn print_byte(byte: u8) {
-    for char_ in ascii::escape_default(byte).map(|b| b as char) {
-        write!(io::stdout(), "{}", char_)
-            .expect("could not write to stdout")
+fn print_byte<W: Write>(byte: u8, writer: &mut W) {
+    for char_ in ascii::escape_default(byte) {
+        writer.write_all(&[char_]).expect("could not write to stdout")
     }
 }
 
-fn print_bytes_from_reader<R: Read>(reader: R) {
+fn print_bytes_from_reader<R: Read, W: Write>(reader: R, writer: &mut W) {
     for byte in io::BufReader::new(reader)
         .bytes()
         .map(|b| b.expect("could not read byte from reader"))
     {
-        print_byte(byte)
+        print_byte(byte, writer)
     }
 }
 
@@ -50,10 +49,12 @@ fn main() {
 
     let file_values = matches.values_of("file");
 
+    let mut stdout = io::BufWriter::new(io::stdout());
+
     let file_values = match file_values {
         Some(f) => f,
         None => {
-            print_bytes_from_reader(io::stdin());
+            print_bytes_from_reader(io::stdin(), &mut stdout);
             return;
         },
     };
@@ -64,12 +65,12 @@ fn main() {
 
     for result in iter {
         match result {
-            Ok(f) => print_bytes_from_reader(f),
+            Ok(f) => print_bytes_from_reader(f, &mut stdout),
             Err(e) => print_error(e),
         }
     }
     // TODO: don't print newline if nothing was written
-    write!(io::stdout(), "\n").expect("could not write to stdout");
+    stdout.write_all(&[b'\n']).expect("could not write to stdout");
 
     // TODO: return failing status code if error
 }
